5f3207267dee31054c03194e95213d005d87fad1
Dhruv <gandhidhruv1724@gmail.com>
2026-03-02 18:20:10 +0530

    fixed chatClient

--- server/src/lib/stream.js
+++ server/src/lib/stream.js
@@ -8,7 +8,7 @@ if(!apiKey || !apiSecret){
     console.error('Stream API key or secret is missing');
 }
 
-export const chatClient=new StreamChat(apiKey,apiSecret);
+export const chatClient=new StreamChat.getInstance(apiKey,apiSecret);
 
 export const upsertStreamUser = async(userData) => {
     try {
