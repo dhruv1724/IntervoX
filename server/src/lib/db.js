bbc4c758a18e047d3ab8a105a1437dd29b90a5a9
Dhruv <gandhidhruv1724@gmail.com>
2026-03-02 08:48:02 +0530

    db and tools setup

---
+++ server/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";    
+import {ENV} from "./env.js";
+// Function to connect to MongoDB
+export const connectDB=async()=>{
+    try {
+        const conn= await mongoose.connect(ENV.DB_URL)
+        console.log("✅Connected to MongoDB: ",conn.connection.host)
+    } catch (error) {
+        console.log("Error connecting to MongoDB",error)
+        process.exit(1) // Exit the process with failure
+        //0 means success and 1 means failure
+    }
+}
